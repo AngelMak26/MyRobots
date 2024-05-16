99e454c2d9279cea3c5f3e1681cb9ebd74531dab
Yael <maka28684@gmail.com>
2024-05-16 14:18:34 -0700

    como el profe

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const searchRobots = async () => {
   try {
-    const response = await axios.get('http://jsonplaceholder.typicode.com/users');
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
     return response.data;
   } catch (error) {
     console.error('Error fetching robots:', error);
