a127a7a5f3fd5cd5552efe4dc992dab332116133
Yael <166560482+AngelMak26@users.noreply.github.com>
2024-05-16 10:24:37 -0700

    si jalo

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const searchRobots = async () => {
     try {
-        const response = await axios.get('http://jsonplaceholder.typicode.com/users');
+        const response = await axios.get('https://robohash.org/');
         return response.data;
     } catch (error) {
         console.error('Error fetching robots:', error);
