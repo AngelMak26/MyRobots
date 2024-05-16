3e2c7cb3fef6f7f25790ea0eae062bb6e674ca95
Yael <166560482+AngelMak26@users.noreply.github.com>
2024-05-16 10:34:47 -0700

    Otraveeeees

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const searchRobots = async () => {
     try {
-        const response = await axios.get('https://api.therobotcompany.com/robots');
+        const response = await axios.get('https://robohash.org/?set=set1&size=200x200');
         return response.data;
     } catch (error) {
         console.error('Error fetching robots:', error);
