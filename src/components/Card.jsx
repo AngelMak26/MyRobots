9fb91535d5c459db44e31cbbe137a7029be9c7a0
Yael <maka28684@gmail.com>
2024-05-16 14:36:51 -0700

    otravesomg

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+// Recibe como prop name e email
+function Card({name, email }){
+    return(
+        <div className="column is-one-quarter">
 
-function Card({ name, email }) {
-  return (
-    <div>
-      <h2>{name}</h2>
-      <p>{email}</p>
-    </div>
-  );
+            <div className="card">
+                <div className="card-image">
+                    <figure className="image is-4by3">
+                    <img
+                        // src necesita el prop name
+                        src={`https://robohash.org/${image}`}
+                        // alt necesita el prop email
+                        alt={`${image}`}
+                    />
+                    </figure>
+                </div>
+                <div className="card-content">
+                    <div className="media">
+                        <div className="media-content">
+                            {name}
+                            <p className="title is-4"> </p>
+                            {email}
+                            <p className="subtitle is-6"> </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default Card;
+export default Card
